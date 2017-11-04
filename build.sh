261aeea622b33efecaf0011c75094755bfd69d62
Drill Bear <vklquevs@gmail.com>
2017-11-04 16:30:08 +0000

    Use /usr/bin/env bash and nix-shell to build

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
-gup="$(nix-build --show-trace --no-out-link -A gup '<nixpkgs>')"
-[ -n "$gup" ] || exit 1
-"$gup/bin/gup" all
+nix-shell -p gup bash python --run "gup all"
 
